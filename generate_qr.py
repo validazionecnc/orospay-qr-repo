a61b4dc0090328a727572c6ab29213fd7fd850f0
validazionecnc <validazione@orospay.it>
2025-09-12 11:05:41 +0200

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -5,13 +5,12 @@ Requisiti: pip install qrcode[pil]
 """
 import qrcode
 
-# Inserisci qui l'URL GitHub Pages del repository
+# Inserisci qui l'URL GitHub Pages del repository con il nuovo index.html
 redirector_url = 'https://validazionecnc.github.io/orospay-qr-repo/'
 
-if not redirector_url:
-    raise SystemExit('Imposta redirector_url nel file generate_qr.py')
-
+# Genera il QR code
 img = qrcode.make(redirector_url)
 output_file = 'orospay-redirect-qr.png'
 img.save(output_file)
-print(f'QR generato in: {output_file}\nContenuto QR: {redirector_url}')
+
+print(f'QR generato in {output_file} con URL: {redirector_url}')
